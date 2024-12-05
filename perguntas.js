35662dd882c05bafa4ee6529ced573a4c7722275
Shai <148061520+ShaiKARALHO@users.noreply.github.com>
2024-12-05 11:45:21 -0300

    xcvxcvxcv

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criarCartao(
+    "qual o item que faz você dar pulo duplo no Hollow Knight",
+    "Asas do Monarca"
+)
+
+criarCartao(
+    "Qual empresa criou o jogo Hollow Knight",
+    "Foi a empresa Team Cherry"
+)
+
+criarCartao(
+    "Como se chama a personagem que te ajuda durante o jogo",
+    "O nome dela é Hornet"
+)
+
